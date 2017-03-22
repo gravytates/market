9f8607b7e4463c1397ab3fb4bde9a62851d5e31d
Grady Shelton and Spener Ruiz <grady@>
2017-03-22 14:56:48 -0700

    print form values into reciept page, delete reciept page, and hide it with the index.html so all info is on one page and no cookies required

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,32 @@
-var color = $("#color").val();
-var customColor = $("#customColor").val();
-var name = $("#name").val();
-var phone = $("#phone").val();
-var streetAddress = $("#streetAddress").val();
-var city = $("#city").val();
-var state = $("#state").val();
-var zipcode = $("#zipcode").val();
-var paymentType = $("#paymentType").val();
+$(document).ready(function() {
+
+  $("#receiptPage").hide();
+
+  $("#submit").click(function() {
+
+    var color = $("#color").val();
+    var customColor = $("#customColor").val();
+    var name = $("#name").val();
+    var phone = $("#phone").val();
+    var streetAddress = $("#streetAddress").val();
+    var city = $("#city").val();
+    var state = $("#state").val();
+    var zipcode = $("#zipcode").val();
+    var paymentType = $("#paymentType").val();
+
+    $("#receiptPage").show();
+    $("#homePage").hide();
+
+    $("#colorRec").text(color);
+    $("#customColorRec").text(customColor);
+    $("#nameRec").text(name);
+    $("#phoneRec").text(phone);
+    $("#streetAddressRec").text(streetAddress);
+    $("#cityRec").text(city);
+    $("#stateRec").text(state);
+    $("#zipcodeRec").text(zipcode);
+    $("#paymentTypeRec").text(paymentType);
+
+  });
+
+});
